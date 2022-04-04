4d06403167887129fd64c20fbe79f506cc835358
sufiks <sufx@rambler.ru>
2022-04-04 18:28:46 +0500

    Lesson 4.10

--- oop.py
+++ oop.py
@@ -1,8 +1,8 @@
-a,b,count=int(input()),int(input()),0
-for i in range(a,b+1):
-    if (i**3)%4==0 or (i**3)%9==0:
-        count+=1
-print(count)
+n=int(input())
+for i in range(n):
+    i=int(input())
+    i+=i
+print(i)
 
 
 

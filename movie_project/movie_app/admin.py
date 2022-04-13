e4e0dc8a2b47e87c7fe5b8998893f879a02d4925
sufiks <sufx@rambler.ru>
2022-04-13 16:57:42 +0500

    Lesson 5.7

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -1,3 +1,38 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from .models import Movie
+from django.db.models import QuerySet
 
 # Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name','rating','currency','budget', 'rating_status']
+    list_editable = ['rating','currency','budget']
+    ordering = ['rating','name']
+    list_per_page = 10
+    actions = ['set_dollars','set_euro']
+    search_fields = ['name__startswitch','rating']
+
+
+    @admin.display(ordering='rating', description='Status')
+    def rating_status(self, mov:Movie):
+        if mov.rating < 50:
+            return 'Зачем это смотреть?'
+        elif mov.rating < 70:
+            return 'Разок можно глянуть'
+        elif mov.rating <= 85:
+            return 'Зачет'
+        return 'Топчик'
+
+    @admin.action(description='Установить валюту в доллар')
+    def set_dollars(self,request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
+
+    @admin.action(description='Установить валюту в Евро')
+    def set_euro(self, request, qs: QuerySet):
+        count_updated = qs.update(currency=Movie.EUR)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updated} записей',
+            messages.ERROR
+        )
--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 from .models import Movie
 # Create your views here.
 
 
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool = Value(False),
+        str_field=Value('hello'),
+        int_field=Value(123),
+        new_budget = F('budget') + 100,
+        new_year = F('year')+F('rating')
+
+
+        )
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'))
     return render(request, 'movie_app/all_movies.html',{
-        'movies': movies
+        'movies': movies,
+        'agg': agg,
+        'total': movies.count()
     })
 
 def show_one_movie(request, slug_movie:str):
